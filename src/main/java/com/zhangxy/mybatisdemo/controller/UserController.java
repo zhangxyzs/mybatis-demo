1702eedfdb8e1c1c209cd583da6d41fb144e0150
zhang_xy <zhangxy@163.com>
2019-10-05 23:03:24 +0800

    用户提交

---
+++ src/main/java/com/zhangxy/mybatisdemo/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.zhangxy.mybatisdemo.controller;
+
+/**
+ * @program: mybatis-demo
+ * @Date: 2019/10/5 23:03
+ * @Author: Zhangxy
+ * @Description:
+ */
+public class UserController {
+}
